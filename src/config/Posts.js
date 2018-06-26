e2335d524ff0daf0d0ffaf25adb1df1e651e81ed
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 19:58:05 -0400

    Create Data Object For Posts

---
+++ src/config/Posts.js
@@ -0,0 +1,24 @@
+// Posts Data
+const Posts = [
+  {
+    id: 1,
+    title: 'Top Sidebar',
+    author: 'Albert Einstein',
+    quote: "The world is a dangerous place to live; not because of the people who are evil, but because of the people who don't do anything about it."
+  },
+  {
+    id: 2,
+    title: 'Middle Sidebar',
+    author: 'Confucius',
+    quote: "Choose a job you love, and you will never have to work a day in your life."
+  },
+  {
+    id: 3,
+    title: 'Bottom Sidebar',
+    author: 'Leonardo Davinci',
+    quote: 'I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.'
+  }
+]
+
+export default Posts
+

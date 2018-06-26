99c7a07c23db79fcc1f6cd4887cc844250a37d27
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 19:58:53 -0400

    Display All Post On Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,18 @@
 import React from 'react'
 
 import Articles from '../config/Articles'
+import Posts from '../config/Posts'
 
 import Wrapper from '../components/Wrapper'
 import Article from '../containers/Article'
+import Post from '../containers/Post'
 
 export default () => (
   <Wrapper body>
     <Wrapper feed>
       { Articles.map(item => <Article key={ item.id } { ...item } />) }
     </Wrapper>
+    { Posts.map(item => <Post key={ item.id } { ...item } />) }
   </Wrapper>
 )
 

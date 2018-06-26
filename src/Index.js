32e15fad67b3460f859581a6201e14b9ddd9e3f2
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 17:21:43 -0400

    Update App With Router Configuration

--- src/Index.js
+++ src/Index.js
@@ -1,8 +1,15 @@
 // Index File
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-const App = () => <div>Hello World</div>
+import Routes from './config/Routes'
+
+const App = () => (
+  <Router>
+    <Routes />
+  </Router>
+)
 
 render(<App />, document.getElementById('app'))
 

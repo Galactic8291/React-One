9045c6e2f454611aa4fce40d0a9f8ac2ce9d75a2
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 19:32:16 -0400

    Display All Articles On Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 // Home Page
 import React from 'react'
 
+import Articles from '../config/Articles'
+
 import Wrapper from '../components/Wrapper'
+import Article from '../containers/Article'
 
 export default () => (
   <Wrapper body>
-    <div>Hello World!!!</div>
+    <Wrapper feed>
+      { Articles.map(item => <Article key={ item.id } { ...item } />) }
+    </Wrapper>
   </Wrapper>
 )
 

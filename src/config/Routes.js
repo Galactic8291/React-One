854e0a86947da177b2d49b3e4c88a548ac6125b6
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 18:21:06 -0400

    Create New Routes For Corresponding Pages

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -3,10 +3,16 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
 import Home from '../pages/Home'
+import About from '../pages/About'
+import Portfolio from '../pages/Portfolio'
+import Contact from '../pages/Contact'
 
 export default () => (
   <Switch>
     <Route exact path='/' component={ Home } />
+    <Route path='/about' component={ About } />
+    <Route path='/portfolio' component={ Portfolio } />
+    <Route path='/contact' component={ Contact } />
   </Switch>
 )
 

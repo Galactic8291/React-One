1274bc0d6ffadb01daac33512798bc73a197cb49
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 19:31:11 -0400

    Create Data Object For Articles

---
+++ src/config/Articles.js
@@ -0,0 +1,27 @@
+// Articles Data
+const Articles = [
+  {
+    id: 1,
+    title: 'First Post',
+    info: 'Info Provided by Robbie',
+    author: 'Charlie Chaplin',
+    quotes: [
+      "Failure is unimportant. It takes courage to make a fool of yourself.",
+      "A tramp, a gentleman, a poet, a dreamer, a lonely fellow, always hopeful of romance and adventure.",
+      "A day without laughter is a day wasted."
+    ]
+  },
+  {
+    id: 2,
+    title: 'Second Post',
+    info: 'Info Provided by Robbie',
+    author: 'Alan Watts',
+    quotes: [
+      "The reason we have poverty is that we have no imagination. There are a great number of people accumulating what they think is vast wealth, but it's only money... they don't know how to enjoy it, because they have no imagination",
+      "To have faith is to trust yourself to the water. When you swim you don't grab hold of the water, because if you do you will sink and drown. Instead you relax and float."
+    ]
+  }
+]
+
+export default Articles
+

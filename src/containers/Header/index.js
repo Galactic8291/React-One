9aa4431b93f8df9ac7572ec3cc7d09f483ac230a
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 18:22:48 -0400

    Update Header With New Components + Full Navigation

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,11 +1,24 @@
 // Header Container
 import React from 'react'
 
+import img from '../../assets/bear.jpg'
 import Header from '../../components/Header'
+import Image from '../../components/Image'
+import Navigation from '../../components/Navigation'
+import { NavLink as Link } from '../../components/Link'
+import Wrapper from '../../components/Wrapper'
 
 export default () => (
   <Header>
-    <div>Testing</div>
+    <Image navigation src={ img } />
+    <Navigation>
+      <Wrapper navigation>
+        <Link exact activeClassName='active' to='/'>Home</Link>
+        <Link exact activeClassName='active' to='/about'>About</Link>
+        <Link exact activeClassName='active' to='/portfolio'>Portfolio</Link>
+        <Link exact activeClassName='active' to='/contact'>Contact</Link>
+      </Wrapper>
+    </Navigation>
   </Header>
 )
 

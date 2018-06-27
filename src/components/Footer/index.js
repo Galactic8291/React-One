0fa6a3a0e6d06ea0df63eb4ed85bf9413c4b76ea
Robbie McClellan <rlm8291@gmail.com>
2018-06-26 20:04:41 -0400

    Create Footer Component With Styles

---
+++ src/components/Footer/index.js
@@ -0,0 +1,18 @@
+// Footer Component
+import styled, { css } from 'styled-components'
+
+export default styled.footer`
+  height: 40px;
+  width: 100%;
+  float: left;
+  border-radius: 5px;
+  background-color: #666;
+  margin: 2% 0;
+
+  p {
+    width: 92%;
+    margin: 10px auto;
+    color: #fff;
+  }
+`
+
